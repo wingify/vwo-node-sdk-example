d4a7bc8c229e7c5f4733f691559db0e1f3ddf9ce
softvar <varun2902@gmail.com>
2019-07-10 16:16:47 +0530

    sample config file which needs to be updated by user for testung NodeJs SDK

---
+++ app/config.js
@@ -0,0 +1,11 @@
+const campaignTestKey = 'REPLACE_THIS_WITH_CORRECT_VALUE';
+const goalIdentifeir = 'REPLACE_THIS_WITH_CORRECT_VALUE';
+const accountId = 'REPLACE_THIS_WITH_CORRECT_VALUE';
+const sdkKey = 'REPLACE_THIS_WITH_CORRECT_VALUE';
+
+module.exports = {
+  accountId,
+  sdkKey,
+  campaignTestKey,
+  goalIdentifeir
+};
